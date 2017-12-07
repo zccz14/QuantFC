3c9aef56ebb20f1384a53823796e8de107b16ebb
zccz14 <zccz14@outlook.com>
2017-12-07 13:12:53 +0800

    init

---
+++ INode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuantFC
+{
+    /// <summary>
+    /// Semantic Only Node
+    /// </summary>
+    public interface INode
+    {
+        string Title { get; }
+        int Count { get; }
+        string GetLabel(int index);
+    }
+
+    public interface IGraphNode: INode
+    {
+        IGraphNode this[int index] { get; set; }
+    }
+
+    public interface IImplementation
+    {
+        int Run();
+    }
+
+    public interface IImplementation<in T>
+    {
+        int Run(T input);
+    }
+}
