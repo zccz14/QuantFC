03ea99bf8dbb69cb3fe8d1c9ea0b54794aa3f3f5
zccz14 <zccz14@outlook.com>
2017-12-11 16:03:48 +0800

    Run !

--- X.Graph.cs
+++ X.Graph.cs
@@ -10,5 +10,17 @@ namespace QuantFC
         public static IEnumerable<INode> ImplementedNodes(this IGraph graph) => graph.Where(node => node.IsImplemented);
 
         public static double ImplementedRate(this IGraph graph) => (double) graph.ImplementedNodes().Count() / graph.Count;
+
+        public static void Run(this IGraph graph)
+        {
+            var node = graph[0];
+            while (node != null)
+            {
+                if (node.IsImplemented)
+                {
+                    node = node.GetNext(((IImplementedNode) node).Run());
+                }
+            }
+        }
     }
 }
